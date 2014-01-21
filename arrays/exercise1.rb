dc127044e755a8a7940943ea4e97eaaf4cf20d97
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-01-21 09:30:21 -0800

    add first exercise ruby file

---
+++ arrays/exercise1.rb
@@ -0,0 +1,7 @@
+# Write a method that takes an array as an argument and 
+# returns another array with all elements that are greater 10 and 
+# doesn’t fail if you pass in non-numbers
+
+[1, 2, 4, 11, 14, 'asdf', false, nil]
+
+def give_my_greater_than_ten
